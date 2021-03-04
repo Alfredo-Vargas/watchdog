9955a29565de39cd41e0e628ef53ba231ecc7674
Harun Kahramanoglu <Kahramanogluharun@gmail.com>
2021-03-04 13:50:27 +0100

    Add project files.

---
+++ WatchDog_V1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace test
+{
+    partial class Loginbox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Loginbox));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Login = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginTxt = new System.Windows.Forms.TextBox();
+            this.PassTxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Login
+            // 
+            this.Login.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Login.AutoSize = true;
+            this.Login.BackColor = System.Drawing.Color.Black;
+            this.Login.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login.ForeColor = System.Drawing.Color.Lime;
+            this.Login.Location = new System.Drawing.Point(196, 179);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(64, 23);
+            this.Login.TabIndex = 1;
+            this.Login.Text = "Login";
+            this.Login.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Login.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(900, 459);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.mm);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(12, 459);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 63);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Black;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(153, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // LoginTxt
+            // 
+            this.LoginTxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginTxt.BackColor = System.Drawing.Color.Black;
+            this.LoginTxt.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginTxt.ForeColor = System.Drawing.Color.Lime;
+            this.LoginTxt.Location = new System.Drawing.Point(308, 179);
+            this.LoginTxt.Name = "LoginTxt";
+            this.LoginTxt.Size = new System.Drawing.Size(200, 23);
+            this.LoginTxt.TabIndex = 5;
+            // 
+            // PassTxt
+            // 
+            this.PassTxt.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PassTxt.BackColor = System.Drawing.Color.Black;
+            this.PassTxt.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PassTxt.ForeColor = System.Drawing.Color.Lime;
+            this.PassTxt.Location = new System.Drawing.Point(308, 253);
+            this.PassTxt.Name = "PassTxt";
+            this.PassTxt.Size = new System.Drawing.Size(200, 23);
+            this.PassTxt.TabIndex = 7;
+            this.PassTxt.TextChanged += new System.EventHandler(this.PassTxt_TextChanged);
+            // 
+            // Loginbox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(1055, 534);
+            this.Controls.Add(this.PassTxt);
+            this.Controls.Add(this.LoginTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.label1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Loginbox";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "WatchDog";
+            this.Load += new System.EventHandler(this.WatchDog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LoginTxt;
+        private System.Windows.Forms.TextBox PassTxt;
+    }
+}
+
---
+++ WatchDog_V1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Loginbox : Form
+    {
+        public Loginbox()
+        {
+            InitializeComponent();
+        }
+
+        private void WatchDog_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mm(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Login = LoginTxt.Text;
+            string pass = PassTxt.Text;
+
+            if (Login == "Harun" && pass == "WatchDog123");
+            else
+            {
+                MessageBox.Show("Password or Login incorrect try again");
+            }
+           
+                 
+            
+        }
+
+        private void PassTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
