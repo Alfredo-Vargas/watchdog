00e6feda6f8429d1b03b23d8feb3226a542d2ed6
Alfredo Vargas <alfredo.vargas-paredes@outlook.com>
2021-03-18 17:14:09 +0100

    18/03/2021 merging1

--- WatchDog_V1/Form1.cs
+++ WatchDog_V1/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WatchDog_V1;
 
@@ -23,25 +16,19 @@ namespace test
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void Label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void mm(object sender, EventArgs e)
+
+
+        private void Label2_Click_1(object sender, EventArgs e)
         {
 
         }
 
-        private void label2_Click_1(object sender, EventArgs e)
-        {
 
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void PassTxt_TextChanged(object sender, EventArgs e)
         {
@@ -53,39 +40,54 @@ namespace test
 
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+     
+
+        private void Button4_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-
+           
             this.Close();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+      
+        private void Button3_Click(object sender, EventArgs e)
         {
-            string Login = LoginTxt.Text;
-            string pass = PassTxt.Text;
+            Login();
+        }
 
-            if (Login == "h" && pass == "w") {
+        private void Login()
+        {
+
+            if (Login == "h" && pass == "w")
+            {
+
                 Form2 main = new Form2();
                 main.Show();
                 this.Hide();
-                
+
+
             }
+
             else
             {
                 MessageBox.Show("Password or Login incorrect try again");
 
             }
+        }
+
+        private void PassTxt_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+            {
+                Login();
+            }
 
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        private void Register_Lable_Click(object sender, EventArgs e)
         {
-
+            Form4 Registration = new Form4();
+            Registration.Show();
+            this.Hide();
         }
     }
 }
